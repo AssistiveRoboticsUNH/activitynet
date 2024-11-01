99cdf74e41942207f2ca3aae1b2e4f3070329053
Noushad Sojib <noushad.sust@gmail.com>
2024-10-31 23:20:04 -0400

    clean

--- activityzed_ws/src/activity_zed/activity_zed/run_rendering.sh
+++ activityzed_ws/src/activity_zed/activity_zed/run_rendering.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Set the ROS_DOMAIN_ID for the first process and run it in the background
-# ROS_DOMAIN_ID=1 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/ashley/cam1/rosbag2_ashley/rosbag2_ashley &
-
-# Set the ROS_DOMAIN_ID for the second process and run it in the background
-# ROS_DOMAIN_ID=2 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/ashley/cam1/rosbag2_ashley_1/rosbag2_ashley_1 &
-
-
-
-ROS_DOMAIN_ID=1 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/skylar/cam1/rosbag2_skylar_18_1/rosbag2_skylar_18_1 &
-ROS_DOMAIN_ID=2 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/skylar/cam1/rosbag2_skylar_18/rosbag2_skylar_18 &
-ROS_DOMAIN_ID=3 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/skylar/cam1/rosbag2_skylar_18_pill/rosbag2_skylar_18_pill &
-ROS_DOMAIN_ID=4 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/ashleigh/cam1/rosbag2_ashleigh_18/rosbag2_ashleigh_18 &
-ROS_DOMAIN_ID=5 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/ashleigh/cam1/rosbag2_ashleigh_18_pill/rosbag2_ashleigh_18_pill &
-ROS_DOMAIN_ID=6 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/ashleigh/cam1/rosbag2_ashleigh_18_1/rosbag2_ashleigh_18_1 &
+ROS_DOMAIN_ID=1 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/skylar/cam4/rosbag2_skylar_18_1/rosbag2_skylar_18_1 &
+ROS_DOMAIN_ID=2 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/skylar/cam4/rosbag2_skylar_18/rosbag2_skylar_18 &
+ROS_DOMAIN_ID=3 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/skylar/cam4/rosbag2_skylar_18_pill/rosbag2_skylar_18_pill &
+ROS_DOMAIN_ID=4 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/ashleigh/cam4/rosbag2_ashleigh_18/rosbag2_ashleigh_18 &
+ROS_DOMAIN_ID=5 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/ashleigh/cam4/rosbag2_ashleigh_18_pill/rosbag2_ashleigh_18_pill &
+ROS_DOMAIN_ID=6 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/ashleigh/cam4/rosbag2_ashleigh_18_1/rosbag2_ashleigh_18_1 &
+ROS_DOMAIN_ID=7 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/ashley/cam4/rosbag2_ashley/rosbag2_ashley &
+ROS_DOMAIN_ID=8 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/ashley/cam4/rosbag2_ashley_1/rosbag2_ashley_1 &
+ROS_DOMAIN_ID=9 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/lauren/cam4/rosbag2_lauren/rosbag2_lauren &
+ROS_DOMAIN_ID=10 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/lauren/cam4/rosbag2_lauren_1/rosbag2_lauren_1 &
+ROS_DOMAIN_ID=11 python3 zed_sub.py --rosbag_path /media/ns/Seagate/oct18/lauren/cam4/rosbag2_lauren_2/rosbag2_lauren_2 &
 
 
 # Wait for both background processes to finish
